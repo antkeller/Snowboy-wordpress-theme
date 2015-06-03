9c01252524f431cb07190d214f53c48ce7a50dda
Anthony Keller <antkeller1@gmail.com>
2015-06-02 20:42:18 -0700

    Add basic home and sub page templates -Home, about, and services

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="featured-slider">
+  <div class="section-content">
+  </div><!-- .section-content -->
+</section><!-- .featured-slider -->
+
+<section class="middle-content">
+  <div class="section-content">
+    <div class="home-about">
+    </div><!-- .home-about -->
+    <div class="home-services">
+    </div><!-- .home-services -->
+    <div class="sidebar">
+    </div><!-- .sidebar -->
+  </div><!-- .section-content -->
+</section><!-- .middle-content -->
+
+<section class="home-galleries">
+  <div class="section-content">
+  </div><!-- .section-content -->
+</section><!-- .home-galleries -->
+
+<section class="home-clients">
+  <div class="section-content">
+  </div><!-- .section-content -->
+</section><!-- .home-clients -->
+<?php get_footer(); ?>
