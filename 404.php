40c9a3a49b3929a96d6ec8e1c6b30901c18f718c
Anthony Keller <antkeller1@gmail.com>
2015-06-26 22:22:02 -0700

    Minor changes to 404 error page

--- 404.php
+++ 404.php
@@ -9,8 +9,12 @@
 
 get_header(); ?>
 
-	<h2>404</h2>
-	<p>Page not found</p>
+<section class="error-404">
+	<div class="section-content">
+		<h2 class="big">404</h2>
+		<p>Page not found. You must be lost.</p>
+	</div>
+</section>
 
 	<?php include get_template_directory() . '/_/inc/modules/social.php'; ?>
 
