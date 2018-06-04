3a9225b7a0bec2306ad3e9734bb07792be3a56ef
Anthony Keller <antkeller1@gmail.com>
2018-06-03 17:43:46 -0700

    Add script version for main js and css files

--- _/inc/footer-includes.php
+++ _/inc/footer-includes.php
@@ -2,11 +2,11 @@
         // Figure out what our server name is
         $host = $_SERVER['SERVER_NAME'];
         // check if we are in the dev environment
-        if ($host == 'localhost' || $host == 'snowboy.dev') {
+        if ($host == 'localhost' || $host == 'snowboy.test') {
             // we're on dev, so include the JavaScript individually for easier debugging
             include 'footer-scripts.php';
         } else {
             // if production, provide the compiled and uglified JS files
-            echo '<script type="text/javascript" src="' . get_template_directory_uri() . '/_/js/snowboy.footer.min.js"></script>' . "\n";
+            echo '<script type="text/javascript" src="' . get_template_directory_uri() . '/_/js/snowboy.footer.min.js?v=' . $GLOBALS['SCRIPT_VERSION'] . '"></script>';
         }
     ?>
