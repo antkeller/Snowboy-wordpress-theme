abb12ff4aed886038a29fa915f823bf4bfacae16
Anthony Keller <antkeller1@gmail.com>
2018-06-03 17:45:15 -0700

    Remove social media links

--- _/inc/modules/social.php
+++ _/inc/modules/social.php
@@ -6,8 +6,8 @@
       <h2 class="section-heading">Follow Us</h2>
       <ul>
         <li title="Instagram" class="instagram"><a href="https://instagram.com/snowboyproductions/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/_/img/instagram-icon.png" alt="Twitter"/></a></li>
-        <li title="Facebook" class="facebook"><a href="https://www.facebook.com/SNOWBOYproductions" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/_/img/facebook-icon.png" alt="Twitter"/></a></li>
-        <li title="Twitter" class="twitter"><a href="https://twitter.com/snowboyus" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/_/img/twitter-icon.png" alt="Twitter"/></a></li>
+        <!-- <li title="Facebook" class="facebook"><a href="https://www.facebook.com/SNOWBOYproductions" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/_/img/facebook-icon.png" alt="Twitter"/></a></li>
+        <li title="Twitter" class="twitter"><a href="https://twitter.com/snowboyus" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/_/img/twitter-icon.png" alt="Twitter"/></a></li> -->
       </ul>
       <div class="row"><?php dynamic_sidebar( 'primary' ); ?></div>
       <!-- <?php echo do_shortcode('[instagram-feed]'); ?> -->
